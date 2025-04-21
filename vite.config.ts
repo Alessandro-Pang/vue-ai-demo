97674191d17f94eab596e6d1b0d54a3cf223c30a
Alessandro-pang <pcy1314@163.com>
2025-04-21 16:51:32 +0800

    chore: 修复 github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+/*
+ * @Author: zi.yang
+ * @Date: 2024-12-10 09:15:22
+ * @LastEditors: zi.yang
+ * @LastEditTime: 2025-04-21 16:50:40
+ * @Description: 
+ * @FilePath: /vue-app/vite.config.ts
+ */
+import { defineConfig } from 'vite';
+
+import vue from '@vitejs/plugin-vue';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig(({ mode }) => {
+  const isProd = mode === 'production';
+  return {
+    base: isProd ? '/vue-ai-demo' : './',
+    plugins: [vue()],
+  }
 })
